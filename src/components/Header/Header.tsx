8fbbb55a67fbf742c9d2e47f73fe9972d061c9fc
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-05-09 14:53:20 +0200

    add moves, like active links, favorites, shopingBsg

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import logoItem from '../../icons/niceGadgets.svg';
 import logoItemOk from '../../icons/Ok.svg';
 import menuOpener from '../../icons/Menu.svg';
-// import favoritesRed from '../../icons/faqvoritesFilled.svg';
+import favoritesRed from '../../icons/faqvoritesFilled.svg';
 import favorites from '../../icons/favourites.svg';
 import shoppingBag from '../../icons/shoppingBag.svg';
 
@@ -30,12 +30,16 @@ const navList = ['home', 'phones', 'tablets', 'accessories'];
 
 export const Header = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [hasFavorites, setHasFavorites] = useState(false);
+  const [expectToBuy, setExpectToBuy] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
       const windowWidth = window.innerWidth;
 
-      setIsMobile(windowWidth > 639);
+      setIsMobile(windowWidth < 640);
+      setHasFavorites(true);
+      setExpectToBuy((prev) => prev + 1);
     };
 
     handleResize();
@@ -50,50 +54,73 @@ export const Header = () => {
     <header className="header" >
       <div className='container'>
         <div className="header__content">
-          <a href="/" className="logo">
-            <img
-              className="logo__image"
-              src={logoItem} alt="Logo icon"
-            />
-            <img className="logo__ok" src={logoItemOk} alt="ok" />
-          </a>
+          <div className="header__right-side">
+            {isMobile && (
+              <a href="/" className="logo">
+                <img
+                  className="logo__image"
+                  src={logoItem} alt="Logo icon"
+                />
+                <img className="logo__ok" src={logoItemOk} alt="ok" />
+              </a>
+            )}
 
-            {isMobile ? (
-              <>
-                <nav className="header__nav nav">
-                  <ul className="nav__panel">
-                    {navList.map(item => (
-                      <li key={item} className="nav__item">
-                        <a href={`/${item}`} className={classNames(
-                          'nav__link',
-                          { 'nav__link--is-active': false },
-                        )}>
-                          {item}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                  </nav>
+            {!isMobile && (
+              <nav className="header__nav nav">
+                <ul className="nav__panel">
+                  <li className='nav__item'>
+                    <a href="/" className="logo">
+                      <img
+                        className="logo__image"
+                        src={logoItem} alt="Logo icon"
+                      />
+                      <img className="logo__ok" src={logoItemOk} alt="ok" />
+                    </a>
+                  </li>
+                  {navList.map(item => (
+                    <li key={item} className="nav__item">
+                      <a href={`/${item}`} className={classNames(
+                        'nav__link',
+                        { 'nav__link--is-active': true },
+                      )}>
+                        {item}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )}
+          </div>
 
-                  <div className="header__buying-section">
+            {!isMobile ? (
+              <div className="header__buying-section">
 
-                  <a href='/' className="header__menu-opener">
-                  <img
-                    className="header__menu-opener_image"
-                    src={favorites}
-                    alt="menu"
-                    />
-                  </a>
+                <a href='/favorites' className="header__menu-opener">
+                {hasFavorites ? (
+                  <img src={favoritesRed}
+                    className="header__menu-opener_image" alt="menu"
+                  />
+                ) : (
+                  <img src={favorites}
+                  className="header__menu-opener_image" alt="menu"
+                  />
+                )}
+                </a>
 
-                  <a href='/' className="header__menu-opener">
+                <a
+                  href='/shopingBag'
+                  className="header__menu-opener"
+                >
                   <img
                     className="header__menu-opener_image"
                     src={shoppingBag}
                     alt="menu"
-                    />
-                  </a>
-                  </div>
-                </>
+                  />
+                  <span className='header__shoping-bag-count'>
+                    {expectToBuy}
+                  </span>
+                </a>
+              </div>
             ) : (
               <a href='/' className="header__menu-opener">
                 <img
@@ -103,18 +130,6 @@ export const Header = () => {
                   />
               </a>
             )}
-
-        {/* <h1 className="header__title">
-          Welcome to Nice Gadgets store!
-        </h1>
-
-        <img className="header__novelty" src={newPhone} alt="New 14 Pro" />
-
-        <div className="header__scroller">
-          <img src={reactangleLight} alt="next" />
-          <img src={reactangleDark} alt="next" />
-          <img src={reactangleDark} alt="next" />
-        </div> */}
         </div>
       </div>
     </header>
