983c626a378ed0da058137fd40a303aa539d888b
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-05-09 12:56:20 +0200

    fix some code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,110 @@
-import React from 'react';
-import newPhone from '../../images/header/headerPhonePic14Pro.png';
+import React, { useEffect, useState } from 'react';
+// import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+// import newPhone from '../../images/header/headerPhonePic14Pro.png';
 import logoItem from '../../icons/niceGadgets.svg';
 import logoItemOk from '../../icons/Ok.svg';
 import menuOpener from '../../icons/Menu.svg';
-import reactangleLight from '../../icons/lightRectangle.svg';
-import reactangleDark from '../../icons/darkReactangle.svg';
+// import favoritesRed from '../../icons/faqvoritesFilled.svg';
+import favorites from '../../icons/favourites.svg';
+import shoppingBag from '../../icons/shoppingBag.svg';
+
+const navList = ['home', 'phones', 'tablets', 'accessories'];
+
+// export type Props = {
+//   to: string;
+//   text: string;
+// };
+
+// export const PageNavLink: FC<Props> = ({ to, text }) => (
+//   <NavLink
+//     to={to}
+//     className={({ isActive }) => classNames(
+//       'nav__link',
+//       { 'nav__link--is-active': isActive },
+//     )}
+//   >
+//     {text}
+//   </NavLink>
+// );
 
 export const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+
+      setIsMobile(windowWidth > 639);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className='container'>
-      <header className="header" >
-        <div className="header__nav">
-            <a href="/" className="logo">
-              <img className="logo__image" src={logoItem} alt="Logo icon" />
-              <img className="logo__ok" src={logoItemOk} alt="ok" />
-            </a>
+    <header className="header" >
+      <div className='container'>
+        <div className="header__content">
+          <a href="/" className="logo">
+            <img
+              className="logo__image"
+              src={logoItem} alt="Logo icon"
+            />
+            <img className="logo__ok" src={logoItemOk} alt="ok" />
+          </a>
 
-            <a href='/' className="header__menu-opener">
-              <img
-                className="header__menu-opener_image"
-                src={menuOpener}
-                alt="menu"
-              />
-            </a>
-        </div>
+            {isMobile ? (
+              <>
+                <nav className="header__nav nav">
+                  <ul className="nav__panel">
+                    {navList.map(item => (
+                      <li key={item} className="nav__item">
+                        <a href={`/${item}`} className={classNames(
+                          'nav__link',
+                          { 'nav__link--is-active': false },
+                        )}>
+                          {item}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                  </nav>
 
-        <h1 className="header__title">
+                  <div className="header__buying-section">
+
+                  <a href='/' className="header__menu-opener">
+                  <img
+                    className="header__menu-opener_image"
+                    src={favorites}
+                    alt="menu"
+                    />
+                  </a>
+
+                  <a href='/' className="header__menu-opener">
+                  <img
+                    className="header__menu-opener_image"
+                    src={shoppingBag}
+                    alt="menu"
+                    />
+                  </a>
+                  </div>
+                </>
+            ) : (
+              <a href='/' className="header__menu-opener">
+                <img
+                  className="header__menu-opener_image"
+                  src={menuOpener}
+                  alt="menu"
+                  />
+              </a>
+            )}
+
+        {/* <h1 className="header__title">
           Welcome to Nice Gadgets store!
         </h1>
 
@@ -35,8 +114,9 @@ export const Header = () => {
           <img src={reactangleLight} alt="next" />
           <img src={reactangleDark} alt="next" />
           <img src={reactangleDark} alt="next" />
+        </div> */}
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 };
