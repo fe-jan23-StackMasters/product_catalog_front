4b3f00b5d0b0ad0d43921faaeb7ae65a68bc9e9e
Vladyslav Kanaiev <nordex83@gmail.com>
2023-05-14 21:10:02 +0300

    complete version without api

--- src/components/PhonesPage/PhonesPage.tsx
+++ src/components/PhonesPage/PhonesPage.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Pagination } from '../Pagination/Pagination';
 import './PhonesPage.scss';
 
-export const PhonesPage:React.FC = () => (
-  <>
-    <h1>PhonesPage</h1>
+export const PhonesPage: React.FC = () => (
+  <div className="phonesPage__container">
+    <h1 className='phonesPage__header'>Mobile phones</h1>
 
     <Pagination />
-  </>
+  </div>
 );
