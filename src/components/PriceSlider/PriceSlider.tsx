7b3a81aa95b088baac39c1682750427503d4c86d
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-05-15 12:20:01 +0300

    fixed priceSlider

--- src/components/PriceSlider/PriceSlider.tsx
+++ src/components/PriceSlider/PriceSlider.tsx
@@ -3,19 +3,28 @@ import { useState } from 'react';
 import './PriceSlider.scss';
 
 export const PriceSlider = () => {
-  const [range, setRange] = useState([0, 5000]);
-  const handleChanges = (event: Event, newValue: any) => {
+  const [range, setRange] = useState<number |number[]>([0, 5000]);
+  const handleChanges = (_event: Event, newValue: number |number[]) => {
     setRange(newValue);
   };
 
+  const rangeMin = Array.isArray(range) ? range[0] : 0;
+  const rangeMax = Array.isArray(range) ? range[1] : 5000;
+
   return (
     <div className="filterPrice">
-      <div className="filterPrice__value">
-        <span className="filterPrice__min">${range[0]}</span>
-        <span className="filterPrice__max">${range[1]}</span>
-      </div>
+      <div className="filterPrice__container">
+        <span className="filterPrice__value">${rangeMin}</span>
 
-      <Slider value={range} onChange={handleChanges} max={5000} size="small" />
+        <Slider
+          value={range}
+          onChange={handleChanges}
+          max={5000}
+          size="small"
+        />
+
+        <span className="filterPrice__value">${rangeMax}</span>
+      </div>
     </div>
   );
 };
