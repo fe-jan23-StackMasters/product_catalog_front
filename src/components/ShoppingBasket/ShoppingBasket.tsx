58479009e90721de781024221256831fdd6262de
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-05-17 15:38:30 +0300

    basket local storage logic reworked

--- src/components/ShoppingBasket/ShoppingBasket.tsx
+++ src/components/ShoppingBasket/ShoppingBasket.tsx
@@ -1,54 +1,17 @@
-import { useMemo, useState } from 'react';
 import { BasketCard } from '../BasketCard';
 import { Button } from '../Button';
 import './ShoppingBasket.scss';
-import { StorageProduct } from '../../types/StorageProduct';
-import { ActionBasket } from '../../types/ActionBasket';
 import { Container } from '../Container';
 import { Link, NavLink } from 'react-router-dom';
 import leftArrov from '../../icons/arrowLeft.svg';
+import { useLocalStorageContext } from '../../context/StorageContext';
 
 export const ShoppingBasket = () => {
-  const [phones, setPhones] = useState<StorageProduct[]>([]);
-  const phonesFromStorage = JSON.parse(localStorage.getItem('cart') || '');
-
-  const handleRemovePhone = (id: string) => {
-    const filteredPhones = phones.filter((product) => product.info.id !== id);
-
-    localStorage.setItem('cart', JSON.stringify(filteredPhones));
-
-    setPhones(filteredPhones);
-  };
-
-  const handleAddOrRemoveQuantity = (phoneId: string, action: ActionBasket) => {
-    const updatedPhones = phones.map((phone) => {
-      if (phone.info.id === phoneId) {
-        if (action === 'add') {
-          phone.quantity = phone.quantity + 1;
-        } else {
-          phone.quantity = +phone.quantity - 1;
-        }
-      }
-
-      return phone;
-    });
-
-    localStorage.setItem('cart', JSON.stringify(updatedPhones));
-
-    setPhones(updatedPhones);
-  };
-
-  useMemo(() => {
-    setPhones(phonesFromStorage);
-  }, []);
-
-  const totalPrice = phones.reduce((acc, curr) => {
-    return acc + curr.info.price * curr.quantity;
-  }, 0);
-
-  const totalItems = phones.reduce((acc, curr) => {
-    return acc + +curr.quantity;
-  }, 0);
+  const {
+    totalPrice,
+    totalQuantity,
+    cartItems,
+  } = useLocalStorageContext();
 
   return (
     <Container>
@@ -59,7 +22,7 @@ export const ShoppingBasket = () => {
 
       <h1 className="title">Cart</h1>
 
-      {!totalItems ? (
+      {!cartItems ? (
         <Container>
           <h2 className="basket__title">Ooops... Your basket is empty</h2>
 
@@ -72,12 +35,10 @@ export const ShoppingBasket = () => {
       ) : (
         <div className="basket">
           <div className="basket__cards">
-            {phones.map((product) => (
+            {cartItems.map((product) => (
               <BasketCard
                 key={product.info.id}
                 product={product}
-                handleRemovePhone={handleRemovePhone}
-                handleAddOrRemoveQuantity={handleAddOrRemoveQuantity}
               />
             ))}
           </div>
@@ -86,7 +47,7 @@ export const ShoppingBasket = () => {
             <span className="basket__total-price">{`$${totalPrice}`}</span>
 
             <span className="basket__total-description">
-              {`Total for ${totalItems} items`}
+              {`Total for ${totalQuantity} items`}
             </span>
 
             <Button width="100%" height="48px" type="btn__add">
